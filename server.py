a983bd5c59a2cda3c7d7f6794e72fbaf9cedd051
raculus <muhjh6@gmail.com>
2023-09-23 18:15:44 +0900

    fix: todo & remove for client

--- server.py
+++ server.py
@@ -15,12 +15,10 @@ def addrToStr(addr):
 
 
 def threaded(client_socket, addr):
-    # TODO: 서버에서 보낸 문제와 클라이언트가 받은 문제가 다름
-
     TimePrint(f"Connected by {addrToStr(addr)}")
     problem = exam.problem()
     TimePrint(f'Send problem "{problem}" to {addrToStr(addr)}')
-    client_socket.send(exam.problem().encode("utf-8"))
+    client_socket.send(problem.encode("utf-8"))
 
     while True:
         try:
@@ -38,10 +36,6 @@ def threaded(client_socket, addr):
                 TimePrint(f"{answer} == {solved}")
             else:
                 TimePrint(f"{answer} != {solved}")
-
-            for client in client_sockets:
-                if client != client_socket:
-                    client.send(data)
 
         except ConnectionResetError as e:
             TimePrint(f"Disconnected by {addrToStr(addr)}")
