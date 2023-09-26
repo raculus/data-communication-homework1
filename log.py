ddcb59bcc49a450a3c62a934c5ff416e939c2976
raculus <muhjh6@gmail.com>
2023-09-26 18:01:07 +0900

    add class Log

--- log.py
+++ log.py
@@ -8,3 +8,15 @@ def TimePrint(msg):
     now = time.ctime()
     msg = f"[{now}]{msg}"
     print(msg)
+
+
+class Log:
+    def __init__(self, filename):
+        self.f = open(filename, "w")
+
+    def write(self, txt):
+        self.f.write(txt)
+        self.f.write("\n")
+
+    def save(self):
+        self.f.close
